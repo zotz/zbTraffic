b5f5b3943639cbd32c049e55da95b78a3386845e
drew Roberts <zotzbro@gmail.com>
2026-08-25 10:39:45 -0400

    misc work aug 24

--- sfb.sh
+++ sfb.sh
@@ -5,17 +5,53 @@
 
 cd ~/projects/zbTraffic
 
+echo 'dropping database'
 rm data/traffic.db
+echo 'running create database'
 python3 -m database.create_database
+# Create initial recorsd for:
+# categories | Uncategorized
+# stations | Default Station
+# programs | Default Program
+# stopsets | Default Stopset
+# salespeople | House
+# We want to run this.
+echo 'running create initial records'
 python3 -m database.create_initial_records
-python3 -m database.seed_database
-# seed2_database creates 3 customers now
-python3 -m database.seed2_database --spot-status No
-# seed2big_database imports the extra csv customers
-python3 -m database.seed2big_database
-python3 -m database.seed_programs_database
-python3 -m database.seed_stopsets --date 2026-08-08
-python3 -m database.seed_separation_rules_database
+# seed_database was only creating categories at the end.
+# python3 -m database.seed_database
+# use the category importer instead
+echo 'running the category importer'
+python3 -m database.import_categories_csv database/data/categories_all.csv
+# seed2_database seeds
+# customers - 3
+# contacts - 3
+# commercials - 3
+# spots - 3 can be scheduled, pending. or 0 - no spots created.
+# The line below was not creating any spots.
+#echo 'running seed2_database'
+#python3 -m database.seed2_database --spot-status No
+#echo 'running seed2big_database'
+#python3 -m database.seed2big_database
+echo 'running customer csv importer'
+python3 -m database.import_customers_csv database/data/customers_big.csv
+
+echo 'running commercial csv importer'
+python3 -m database.import_commercials_csv database/data/commercials_big.csv
+# stop using seed_programs_database
+# python3 -m database.seed_programs_database
+# programs importer instead
+echo 'running program csv importer'
+python3 -m database.import_programs_csv database/data/programs_all.csv
+# stop using seed_stopsets
+#python3 -m database.seed_stopsets --date 2026-08-08
+# And start using stopsets importer instead
+echo 'running stopset csv importer'
+python3 -m database.import_stopsets_csv database/data/stopsets_all.csv
+
+
+
+#python3 -m database.seed_separation_rules_database
 python3 -m database.generate_avails_range 1 2026-01-01 2026-12-30
 
 
@@ -23,105 +59,42 @@ python3 -m database.generate_avails_range 1 2026-01-01 2026-12-30
 #python3 -m database.seed_spots_database --date 2026-08-08 --spot-status Pending
 #python3 -m database.seed_spots_database --date 2026-08-09 --spot-status Pending
 
-python3 -m database.seed_contracts_database
-python3 -m database.seed_contract_items_database
-# make a contract item rule:
-python3 -m commands.contract_item_rule_add \
-    --contract-item 1 \
-    --days Mon \
-    --start 08:00:00 \
-    --end 10:00:00
-    
-python3 -m commands.contract_item_rule_edit 1 \
-    --days Mon \
-    --start 08:00:00 \
-    --end 10:00:00 \
-    --program 2
-    
-python3 -m commands.contract_item_rule_edit 1 \
-    --days Mon \
-    --start 08:00:00 \
-    --end 10:00:00 \
-    --program 2 \
-    --spots-per-week 2
+#python3 -m database.seed_contracts_database
+python3 -m database.import_contracts_csv database/data/contracts_seed.csv
 
-python3 -m commands.contract_item_rule_edit 1 \
-    --days Mon,Tue,Wed,Thu,Fri \
-    --start 08:00:00 \
-    --end 10:00:00 \
-    --program 2 \
-    --spots-per-week 2
+# python3 -m database.seed_contract_items_database
+python3 -m database.import_contract_items_csv database/data/contract_items_seed.csv
 
-python3 -m commands.contract_item_rule_edit 1 \
-    --days Mon \
-    --start 08:00:00 \
-    --end 10:00:00 \
-    --program 2 \
-    --spots-per-week 2
-
-python3 -m commands.contract_item_rule_edit 1 \
-    --days Mon,Tue,Wed,Thu,Fri \
-    --start 08:00:00 \
-    --end 10:00:00 \
-    --program 2 \
-    --spots-per-day 1 \
-    --spots-per-week 2
-    
-python3 -m commands.contract_item_rule_add \
-    --contract-item 3 \
-    --days Mon,Wed,Fri \
-    --start 12:00:00 \
-    --end 17:00:00 \
-    --program 3 \
-    --spots-per-day 2 \
-    --spots-per-week 5
 
 
 ########### Seed big contracts suite
-python3 -m database.import_categories_csv database/data/categories_big.csv
 python3 -m database.import_separation_rules_csv database/data/separation_rules_big.csv
 python3 -m database.import_contracts_csv database/data/contracts_big.csv
 python3 -m database.import_contract_items_csv database/data/contract_items_big.csv
 python3 -m database.import_contract_item_rules_csv database/data/contract_item_rules_big.csv
 
+# take these out so that no spots are scheduled
+python3 -c "from traffic.scheduler import schedule_contract_item_quantity; print(schedule_contract_item_quantity(1))"
+python3 -c "from traffic.scheduler import schedule_contract_item_quantity; print(schedule_contract_item_quantity(3))"
+
+# No scheduling please, we will do this by hand to try and see what is going wrong.
 for id in $(seq 5 20); do
     echo "===== Contract Item $id ====="
     python3 -c "from traffic.scheduler import schedule_contract_item_quantity; print(schedule_contract_item_quantity($id))"
 done
 
-# take these out so that no spots are scheduled
-python3 -c "from traffic.scheduler import schedule_contract_item_quantity; print(schedule_contract_item_quantity(1))"
-python3 -c "from traffic.scheduler import schedule_contract_item_quantity; print(schedule_contract_item_quantity(3))"
 
-
-# call regression test here.
+# call regression test here. only call in sfb.sh not in sfbnosched.sh
 ./tests/scheduler_regression_test.sh
 
 
-# Add another contract_item_rule
-#python3 -m commands.contract_item_rule_add \
-#    --contract-item 1 \
-#    --program 2
 
-
-
-
-
-# Don't do these 3 right now. They create and assign spots to avails
-# This means the resulting spots end with a status of Scheduled
-#python3 -m commands.spots_generate 1
-#python3 -m commands.spots_generate 3
-#python3 -m commands.spots_generate 4
-# Do these 3 instead. They create spots with a Pending status intended to be manually
-# scheduled with python3 -m prototype.traffic_board
-#python3 -m database.seed_spots_database --date 2026-08-09 --spot-status Pending
-#python3 -m database.seed_spots_database --date 2026-08-10 --spot-status Pending
-#python3 -m database.seed_spots_database --date 2026-08-11 --spot-status Pending
-
+# this is how we export logs right now
 #python3 -m commands.export_rivendell_log 2026-08-07
 #python3 -m commands.export_rivendell_log 2026-08-08
 #python3 -m commands.export_rivendell_log 2026-08-09
 
+# This is how we copy logs to the rivendell machine
 #scp logs/zbt_*.log rd@192.168.86.137:/home/rd/Desktop/ewxfer/zbt/
 
 # Some useful commands
